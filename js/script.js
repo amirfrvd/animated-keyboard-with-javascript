190d6146f6c1bcdc1ebeb3cb2e8e036dc196ba3e
amir mohammad firoozvand <firoozvand655@gmail.com>
2024-03-27 01:36:52 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const titleElem = document.querySelector('.title')
+
+document.addEventListener('keyup', function (event) {
+    event.preventDefault
+
+    appendValueToDom(event)
+
+    let userEventKey = event.key.toUpperCase() // 's a d'
+    let mainKeyElem = document.getElementById(userEventKey)
+
+    mainKeyElem.classList.add('hit')
+
+    mainKeyElem.addEventListener('animationend', function () {
+        mainKeyElem.classList.remove('hit')
+    })
+
+    console.log(mainKeyElem);
+})
+
+
+function appendValueToDom(event) {
+    console.log(event);
+
+    if (event.code === 'Backspace') {
+        titleElem.innerHTML = titleElem.innerHTML.slice(0, -1)
+        return
+    }
+
+    titleElem.innerHTML += event.key
+}
